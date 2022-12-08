2a6b024560245552f686749c0c7ce741452972e6
nmbrah <nanambrah22@gmail.com>
2022-12-08 11:04:12 -0800

    preprocessor

--- 0x00-hello_world/README.md
+++ 0x00-hello_world/README.md
@@ -1,1 +1,1 @@
-C programming
+I can do tthis 
--- 0x00-hello_world/main.c
+++ 0x00-hello_world/main.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
 /**
- * main - Entry point
  *
- * Return: Always 0 (success)
+ *  * main - Entry point
  *
- * */
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
 
 int main(void)
+
 {
-	return (0);
+
+	    return (0);
 
 }
