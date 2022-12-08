7af74a5a6e9b4c560f79b63a03223a8bd269dae3
nmbrah <nanambrah22@gmail.com>
2022-12-08 12:21:16 -0800

    name

---
+++ 0x00-hello_world/4-put.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (sucess)
+ *
+ * */
+
+int main(void)
+{
+	put("\" Programming is like a multlingual puzzle");
+	return (0);
+}
