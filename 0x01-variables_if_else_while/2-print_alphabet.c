f530c5dd73a0797902e7e7726f774e6224e5300b
nmbrah <nanambrah22@gmail.com>
2022-12-09 15:34:07 -0800

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always return 0 (sucess)
+ *
+ */
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+
+	putchar('\n');
+	return (0);
+}
